e76b2f81a7637e8726577b610342af538b92f95c
Nguyen Hai Ha <td12a11haiha@gmail.com>
2021-09-13 17:07:17 +0700

    3

--- config/bot.js
+++ config/bot.js
@@ -8,7 +8,7 @@ module.exports = {
     },
 
     discord: {
-        token: 'ODg2ODkzMjc2NTUxMDY5NzQ2.YT8Nug.Mj-q3cVsB5cVEEr7uLXbGR1gaTQ',
+        //token: 'ODg2ODkzMjc2NTUxMDY5NzQ2.YT8Nug.pb-cAJLdMXQZc9t4p2OLgxgpvUQ',
         prefix: '7',
         activity: 'Music',
     },
