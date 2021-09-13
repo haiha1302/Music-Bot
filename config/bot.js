b30039393bf745b740eb0f6fb5a3847dcaa6a074
Nguyen Hai Ha <td12a11haiha@gmail.com>
2021-09-13 16:29:27 +0700

    first commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODg2ODkzMjc2NTUxMDY5NzQ2.YT8Nug.sqN-40pq1o5Cuoqwu3cnM5bdeUE',
+        prefix: '7',
+        activity: 'Music',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
