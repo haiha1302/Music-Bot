f96f8843091b3d7ec801a9c22860b4a244458863
Nguyen Hai Ha <td12a11haiha@gmail.com>
2021-11-16 16:28:58 +0700

    update timeout 5p

--- player/botDisconnect.js
+++ player/botDisconnect.js
@@ -1,3 +1,5 @@
 module.exports = (client, message, queue) => {
-    message.channel.send(`${client.emotes.error} - Music stopped as i have been disconnected from the channel !`);
+    setInterval(() => {
+        message.channel.send(`${client.emotes.error} - Music stopped as i have been disconnected from the channel !`);
+    }, 5000);
 };
